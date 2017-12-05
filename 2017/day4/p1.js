bc271b5e36167177a5aff55ca427780bf759e1be
Raymond Camden <raymondcamden@gmail.com>
2017-12-04 20:43:18 -0600

    day 4

---
+++ 2017/day4/p1.js
@@ -0,0 +1,31 @@
+
+/*
+let passphrases = [
+    'aa bb cc dd ee',
+    'aa bb cc dd aa',
+    'aa bb cc dd aaa'
+];
+*/
+const fs = require('fs');
+let passphrases = fs.readFileSync('./passphrases.txt', 'UTF8').split('\n');
+
+let totalGood = 0;
+
+passphrases.forEach(p => {
+    if(isGood(p)) {
+        totalGood++;
+        console.log(p+': GOOD!');
+    } else console.log(p+': bad');
+});
+console.log('Total good is '+totalGood);
+
+function isGood(p) {
+    let words = p.split(/\s/);
+    let used = [];
+    for(var i=0;i<words.length;i++) {
+        let word = words[i];
+        if(used.indexOf(word) !== -1) return false;
+        used.push(word);
+    };
+    return true;
+}
