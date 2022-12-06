db7388c1b7a3b1ffa5361e416ae41bab89a93f34
Raymond Camden <raymondcamden@gmail.com>
2022-12-06 17:06:17 -0600

    day5

---
+++ 2022/5/part2.py
@@ -0,0 +1,66 @@
+"""
+I'm going to keep input to directions, and hard code the initial stacks. I think.
+I KNOW I'm going to typo this.
+"""
+
+stacks = []
+stacks.append(["M","J","C","B","F","R","L","H"])
+stacks.append(["Z","C","D"])
+stacks.append(["H","J","F","C","N","G","W"])
+stacks.append(["P","J","D","M","T","S","B"])
+stacks.append(["N","C","D","R","J"])
+stacks.append(["W","L","D","Q","P","J","G","Z"])
+stacks.append(["P","Z","T","F","R","H"])
+stacks.append(["L","V","M","G"])
+stacks.append(["C","B","G","P","F","Q","R","J"])
+
+#stacks.append(["Z","N"])
+#stacks.append(["M","C","D"])
+#stacks.append(["P"])
+
+
+print(stacks)
+
+input2 = [
+"move 1 from 2 to 1",
+"move 3 from 1 to 3",
+"move 2 from 2 to 1",
+"move 1 from 1 to 2"
+]
+
+input = [line.rstrip() for line in open('input.txt','r')]
+
+def move(stacks, fromX, toX, total):
+	curr = 1
+	newCol = ""
+	while curr <= total:
+		topBox = stacks[fromX-1].pop()
+		newCol = topBox + newCol
+		curr = curr + 1
+
+	for box in newCol:
+		stacks[toX-1].insert(len(stacks[toX-1]), box)
+
+
+def topBox(stacks):
+	result = ""
+	for s in stacks:
+		result = result + s.pop()
+
+	return result
+
+for instruction in input:
+	moveIns = instruction.split(" from ")
+	moveTotal = int(moveIns[0].split(" ")[1])
+	fromStack,toStack = moveIns[1].split(' to ')
+
+	fromStack = int(fromStack)
+	toStack = int(toStack)
+
+	print(f"Move {moveTotal} boxes from {fromStack} to {toStack}")
+	move(stacks, fromStack, toStack, moveTotal)
+	#print(stacks)
+
+print('----------------------')
+#print(stacks)
+print(topBox(stacks))
