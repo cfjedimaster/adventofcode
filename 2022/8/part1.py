ac6bf53740c811b29c4f47e827d8eaa2ab1bca98
Raymond Camden <raymondcamden@gmail.com>
2022-12-15 08:29:08 -0600

    misc

--- 2022/8/part1.py
+++ 2022/8/part1.py
@@ -15,6 +15,8 @@ for i in input:
 		toAdd.append(c)
 	forest.append(toAdd)
 
+print(forest)
+
 totalVisible = 0
 for idx,row in enumerate(forest):
 	# we can ignore top and bottom
@@ -22,7 +24,35 @@ for idx,row in enumerate(forest):
 		for idy,col in enumerate(row):
 			# we can ignore beg and end
 			if idy != 0 and idy != len(row)-1:
+				print(f"checking tree {idx},{idy} {forest[idx][idy]} and row: {row} and col: {col}")
 				#
 				# So - now need to check my row and col, Ray, return here
 				#
-				print(idx,idy,"=",col)
+				isVisible = True
+				# first, up and down
+				for x in range(0, idx):
+					print(f"DEBUG - idx, idy, x {idx},{idy},{x}")
+					print(f"UP: checking tree at {x},{idy} - val is {forest[x][idy]}")
+					if forest[x][idx] >= forest[idx][idy]:
+						isVisible = False
+
+				for x in range(idx+1, len(forest[idx])):
+					print(f"DOWN: checking tree at {x},{idy} val is {forest[x][idy]}")
+					if forest[x][idx] >= forest[idx][idy]:
+						isVisible = False
+
+				# now we check row, left and right
+				#print(f"row is {row}")
+				for x,v in enumerate(row):
+					if x != idx:
+						print(f" checking x {x} pos val is {v} my real x is {idx}")
+						print(f" val is {forest[idx][x]}")
+						if forest[idx][x] >= forest[idx][idy]:
+							isVisible = False
+
+				print(idx,idy,"=",col, " and is it visible?", isVisible)
+				if isVisible == True:
+					totalVisible = totalVisible + 1
+
+print("------------")
+print(f"Total ",totalVisible)
